6bbdca0215a7e90ee3d19c32c74d7dbc596541fb
yanlima07 <yan_lima@outlook.com>
2021-08-14 14:38:26 -0300

    classes basicas ADD

---
+++ APOO/src/main/java/Mouse.java
@@ -0,0 +1,26 @@
+public class Mouse extends Produtos {
+	
+	private String dpiMou;
+	private String fioMou;
+	private String corMou;
+	
+	public String getDpiMou() {
+		return this.dpiMou;
+	}
+	public void setDpiMou(String dpiMou) {
+		this.dpiMou = dpiMou;
+	}
+	public String getFioMou() {
+		return this.fioMou;
+	}
+	public void setFioMou(String fioMou) {
+		this.fioMou = fioMou;
+	}
+	public String getCorMou() {
+		return this.corMou;
+	}
+	public void setCorMou(String corMou) {
+		this.corMou = corMou;
+	}
+	
+}
---
+++ APOO/src/main/java/Notebook.java
@@ -0,0 +1,48 @@
+public class Notebook extends Produtos  {
+	
+	private String soNot;
+	private String cpuNot;
+	private String ramNot;
+	private String hddNot;
+	private String ssdNot;
+	private String gpuNot;
+	
+	public String getSoNot() {
+		return this.soNot;
+	}
+	public void setSoNot(String soNot) {
+		this.soNot = soNot;
+	}
+	public String getCpuNot() {
+		return this.cpuNot;
+	}
+	public void setCpuNot(String cpuNot) {
+		this.cpuNot = cpuNot;
+	}
+	public String getRamNot() {
+		return this.ramNot;
+	}
+	public void setRamNot(String ramNot) {
+		this.ramNot = ramNot;
+	}
+	public String getHddNot() {
+		return this.hddNot;
+	}
+	public void setHddNot(String hddNot) {
+		this.hddNot = hddNot;
+	}
+	public String getSsdNot() {
+		return this.ssdNot;
+	}
+	public void setSsdNot(String ssdNot) {
+		this.ssdNot = ssdNot;
+	}
+	public String getGpuNot() {
+		return this.gpuNot;
+	}
+	public void setGpuNot(String gpuNot) {
+		this.gpuNot = gpuNot;
+	}
+	
+	
+}
